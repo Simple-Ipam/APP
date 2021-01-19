17ae5347a49fd3d84e40d33033627f773a71874a
COL1_3439 <57326899+Steve557mag-fr@users.noreply.github.com>
2021-01-19 17:09:01 +0100

    102 - INIT 2 VERSION

---
+++ SimpleIPAM/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SimpleIPAM
+{
+    partial class LoginSIPAM
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginSIPAM));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lgin_btn = new System.Windows.Forms.Button();
+            this.Title_scr = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // lgin_btn
+            // 
+            resources.ApplyResources(this.lgin_btn, "lgin_btn");
+            this.lgin_btn.Name = "lgin_btn";
+            this.lgin_btn.UseVisualStyleBackColor = true;
+            this.lgin_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Title_scr
+            // 
+            resources.ApplyResources(this.Title_scr, "Title_scr");
+            this.Title_scr.Name = "Title_scr";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.ForeColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // LoginSIPAM
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Title_scr);
+            this.Controls.Add(this.lgin_btn);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginSIPAM";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button lgin_btn;
+        private System.Windows.Forms.Label Title_scr;
+        public System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SimpleIPAM/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleIPAM
+{
+    public partial class LoginSIPAM : Form
+    {
+
+        public string userName = "";
+        public StateProgram prgState = StateProgram.USERN_LOG;
+        const string API = "https://simpleipam-api/";
+        public LoginSIPAM()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (prgState)
+            {
+                case StateProgram.USERN_LOG:
+                    // VERIFY IF THIS USERNAME IS VALID 
+                    var result_isAUser = true;
+                    // IF IS TRUE, SET THE PROGRAM STATE TO THE PASSWORD ENTRY
+                    if (result_isAUser) prgState = StateProgram.PASWO_LOG;
+                    else return;
+                    break;
+                case StateProgram.PASWO_LOG:
+                    // VERFIFY THE PASSWORD
+                    //TRY TO CONNECT
+                    break;
+            }
+        }
+
+        public enum StateProgram
+        {
+            USERN_LOG = 0,
+            PASWO_LOG = 1,
+            CONNECTED = 2
+        };
+
+    }
+}
